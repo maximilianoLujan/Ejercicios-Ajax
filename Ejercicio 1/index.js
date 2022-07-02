2cf11d0026db0f8c9b2e110c58d67d0447f76b60
Maximiliano <maxylujan1@outlook.com>
2022-07-02 16:32:20 -0300

    Creando un sitio web con ajax

---
+++ Ejercicio 1/index.js
@@ -0,0 +1,40 @@
+(()=>{
+    const d = document,
+    $main = d.querySelector("main");
+
+    const getHTML = (options) =>{
+        let{url,success,error} = options;
+        let xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", e =>{
+            if (xhr.readyState !== 4) return;
+            if (xhr.status >= 200 && xhr.status < 300){
+                let html = xhr.responseText;
+                success(html);
+            } else{
+                let message = xhr.statusText || "Ha ocurrido un error";
+                error(`Error ${xhr.status}: ${message}`);
+            }
+        })
+        xhr.open("GET",url);
+        xhr.setRequestHeader("Content-type","text/html; charset=utf-8");
+        xhr.send();
+    }
+    d.addEventListener("DOMContentLoaded", e=>{
+        getHTML({
+            url: "assets/home.html",
+            success:(html)=> $main.innerHTML = html,
+            error: (err)=>$main.innerHTML = `<h1>${err}</h1>`
+        });
+    });
+    d.addEventListener("click", e =>{
+        if(e.target.matches(".enlaces a")){
+            e.preventDefault();
+            getHTML({
+                url:e.target.href,
+                success:(html)=> $main.innerHTML = html,
+                error: (err)=>$main.innerHTML = `<h1>${err}</h1>`
+
+            })
+        }
+    })
+})();

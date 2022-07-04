c3f810767a8c52bde46186382c071b60c47592e3
Maximiliano <maxylujan1@outlook.com>
2022-07-04 16:49:31 -0300

    Trayendo datos de la API de Marvel

--- Ejercicio 3/index.js
+++ Ejercicio 3/index.js
@@ -1,10 +1,7 @@
 (()=>{
-    const d = document,
-    $miformulario = d.querySelector(".formulario"),
-    $archivos = d.getElementById("input-file");
-
-    d.addEventListener("submit", e =>{
-        e.preventDefault();
-        console.log($archivos)
-    })
+    const d = document;
+    fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=be70adc9513812f8307fecfa5198f557&hash=8bf1279fdb554f559221ca138f60903e")
+    .then(response => response.json())
+    .then(json=> console.log(json))
+    .catch(err => console.log(err))
 })()

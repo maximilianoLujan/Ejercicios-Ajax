9f5c17faac6759acc77f4319f1fc81c6db857c02
Maximiliano <maxylujan1@outlook.com>
2022-07-06 13:55:53 -0300

    Terminando pagina que trae datos de la api POKEMON

--- Ejercicio 5/index.js
+++ Ejercicio 5/index.js
@@ -2,27 +2,32 @@
     const d = document,
      $mitemplate = d.getElementById("template-pokemon").content,
      $pokemons = d.querySelector(".pokemons"),
-     $mifragment = d.createDocumentFragment();
+     $mifragment = d.createDocumentFragment(),
+     $botonN = d.getElementById("next"),
+     $botonB = d.getElementById("back"),
+     $pag = d.getElementById("pag"),
+     $div = d.querySelector(".loader");
+     let pagina = 0;
 
-    const getAllPokemons = async  () =>{
-        await fetch("https://pokeapi.co/api/v2/pokemon")
+
+    const getAllPokemons = async  (pagina) =>{
+        await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${pagina}&limit=20`)
         .then( (res) => res.json())
         .then(json => {
+            console.log(json)
 
             let pokemones = json.results;
             pokemones.forEach(pok =>{
-                const $clone = d.importNode($mitemplate,true),
+                let $clone = d.importNode($mitemplate,true),
                 $fig = $clone.querySelector(".pokemon"),
                 $img = $fig.querySelector("img"),
                 $fc = $fig.querySelector("figcaption");
                 $fc.innerHTML = `<h4 style="text-align:center;">${pok.name}</h4>`;
-                $img.setAttribute("alt",pok.name)
-                console.log(pok)
+                $img.setAttribute("alt",pok.name);
                 let url = pok.url;
                  fetch(url)
                  .then(res => res.json())
                  .then(json =>{
-                    console.log(json)
                     const urlimg = json.sprites.back_default;
                     $img.setAttribute("src",urlimg);
                  })
@@ -41,6 +46,40 @@
         })
     }
     d.addEventListener("DOMContentLoaded", e =>{
-        getAllPokemons();
+      getAllPokemons(pagina);
+        d.addEventListener("click", e =>{
+            if (e.target == $botonB){
+                if (pagina === 0){
+                    alert("No hay mas paginas hacia atras");
+                } else{
+                    borrarFiguras();
+                    pagina-=20;
+                    $div.classList.toggle("active");
+                    getAllPokemons(pagina);
+                    setTimeout(() => {
+                        $div.classList.toggle("active");
+                    }, 1500);
+                    $pag.value = `${pagina/20}`
+                }
+                if (pagina !== 0) $botonB.disabled = true;
+            }
+            if (e.target == $botonN){
+                borrarFiguras();
+                pagina+=20;
+                $div.classList.toggle("active");
+                getAllPokemons(pagina);
+                setTimeout(() => {
+                    $div.classList.toggle("active");
+                }, 1000);
+                $pag.value = `${pagina/20}`
+            }
+            if (pagina !== 0) $botonB.disabled = false;
+        })
     });
+    const borrarFiguras = () =>{
+        $figuras = d.querySelectorAll("figure");
+            $figuras.forEach(fi=>{
+                fi.remove()
+            })
+    }
 })();
